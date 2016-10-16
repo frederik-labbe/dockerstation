970c6a9bb9dd45caebbdf75bd650bdadbad8b64b
Frederik Labbe <frederik.labbe@gmail.com>
2016-10-16 07:43:01 -0400

    Fix indentation

--- connection/bridge_server.js
+++ connection/bridge_server.js
@@ -5,28 +5,28 @@ var DockerImages = require('../bridge/images');
 var DockerContainers = require('../bridge/containers');
 
 var BridgeServer = function(port) {
-	this.port = port;
+    this.port = port;
 };
 
 BridgeServer.prototype.start = function() {
-	var server = http.createServer(function(request, response) {
-		response.writeHead(404);
-		response.end();
-	});
-	server.listen(this.port);
+    var server = http.createServer(function(request, response) {
+        response.writeHead(404);
+        response.end();
+    });
+    server.listen(this.port);
     
     console.log('Bridge server listening on port ' + this.port);
 
-	wsServer = new WebSocketServer({
-		httpServer: server,
-		autoAcceptConnections: false
-	});
+    wsServer = new WebSocketServer({
+        httpServer: server,
+        autoAcceptConnections: false
+    });
 
-	wsServer.on('request', function(request) {
-		var connection = request.accept('echo-protocol', request.origin);
-		DockerImages.queryList(connection);
-		DockerContainers.queryList(connection);
-	});
+    wsServer.on('request', function(request) {
+        var connection = request.accept('echo-protocol', request.origin);
+        DockerImages.queryList(connection);
+        DockerContainers.queryList(connection);
+    });
 };
 
 module.exports = BridgeServer;
--- routes.js
+++ routes.js
@@ -3,14 +3,14 @@ var Routes = {};
 Routes.applyTo = function(app, bridge_port) {
     app.get('/', function (req, res) {
         res.render('index', {
-			ip: req.hostname,
-			port: bridge_port
-		});
+            ip: req.hostname,
+            port: bridge_port
+        });
     });
     
     app.get('/console/:dockerId', function(req, res) {
-		res.render('console', req.params);
-	});
+        res.render('console', req.params);
+    });
 };
 
 module.exports = Routes;
